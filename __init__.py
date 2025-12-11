ae7214a4716967da589e6742eb0f7101fe4b11c3
heyuan777 <heyuanzhuang357@gmail.com>
2025-12-11 17:42:57 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-maple_foliage package
-
-Provides:
-- ClimateData for daily climate reading
-- WeeklyClimateDataset for feature engineering
-- Labeling tools for peak foliage detection
-- FoliageModel for prediction
-"""
